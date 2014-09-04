fe0ddde10985d5503f1f04dfe9fefe335b93c5fa
won <wjin@nyit.edu>
2014-09-03 23:52:49 -0400

    add full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,10 +12,17 @@ describe "Static pages" do
 		end
 	
 
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("#{base_title} | Home")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
 		end
+
+		it "should not have a custon page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
+		end
+	
+
 	end
 
 
@@ -27,10 +34,16 @@ describe "Static pages" do
 		end
 	
 
-		it "should have the title 'Help" do
+		it "should have the base title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("#{base_title} | Help")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
 		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/help'
+			expect(page).not_to have_title('| Help')
+		end
+
 	end
 
 
@@ -41,11 +54,18 @@ describe "Static pages" do
 			expect(page).to have_content('About Us')
 		end
 
-		it "should have the title 'About Us'" do
+		it "should have the base title" do
 			visit '/static_pages/about'
-			expect(page).to have_title("#{base_title} | About Us")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
 		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/about'
+			expect(page).not_to have_title('| About Us')
+		end
+
 	end
+
 
 
 	describe "Contact page" do
@@ -55,9 +75,14 @@ describe "Static pages" do
 			expect(page).to have_content('Contact Us')
 		end
 
-		it "should have the title 'Contact Us'" do
+		it "should have the base title" do
 			visit '/static_pages/contact'
-			expect(page).to have_title("#{base_title} | Contact Us")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/contact' 
+			expect(page).not_to have_title('| Contact Us')
 		end
 	end
 
